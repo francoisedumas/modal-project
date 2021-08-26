221e0449b36d9fa53783d0b7534167098d55c52c
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-08-26 12:21:01 +0200

    Create a modal with VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{ title }}</h1>
+  <input type="text" ref="name">
+  <p>Nothing here!</p>
+  <button @click="handleClick">Click me</button>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <!-- here is a named slot -->
+      <template v-slot:links>
+        <a href="#">Sign in</a>
+      </template>
+      <!-- here is a slot -->
+      <h1>Ninja in a slot</h1>
+      <p>Cool slot</p>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>This is my second modal</h1>
+      <p>Oh yeahhh</p>
+    </Modal>
+  </div>
+  <p>Welcome...</p>
+  <button @click.shift="toggleModal">Open modal (shift)</button>
+  <p>You like modals...</p>
+  <button @click="toggleModalTwo">Open the second modal</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue App',
+      header: 'Sign up for discount',
+      text: 'Grab your swag',
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
@@ -22,5 +68,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #2c3e50;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
